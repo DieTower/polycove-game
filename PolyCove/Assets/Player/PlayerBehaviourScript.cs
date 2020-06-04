f44defdbdb9d6eb00eb48f7f6a5d5a4e31a0897c
Rodrigo Costa <30880980+DieTower@users.noreply.github.com>
2020-06-04 20:51:18 +0100

    Player

--- PolyCove/Assets/Player/PlayerBehaviourScript.cs
+++ PolyCove/Assets/Player/PlayerBehaviourScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerBehaviourScript : MonoBehaviour
 {
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.animator = this.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            this.animator.SetBool("walkForward", true);
+        }
+        else
+        {
+            this.animator.SetBool("walkForward", false);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.animator.SetBool("walkBackward", true);
+        }
+        else
+        {
+            this.animator.SetBool("walkBackward", false);
+        }
     }
 }
